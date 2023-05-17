56114b983f3e1a1cc9994844dae14a21b1498cb6
Naveen CS <catchspider2002@gmail.com>
2023-05-17 11:28:13 +0530

    Added new patterns and 2 new languages

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,5 +1,4 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 export const themeStore = writable("light");
 export const langStore = writable("pt");
-export const webStore = writable("https://pt.pattern.monster");
